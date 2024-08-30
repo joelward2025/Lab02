5d2db3f141ca840b0c75ef135cf9aac055692723
Joel Ward <joelward@cedarville.edu>
2024-08-30 16:05:05 -0400

    init commit

--- Lab02/ContentView.swift
+++ Lab02/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Text("CHOOSE\nYOUR PLAN")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .multilineTextAlignment(.center)
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                PricingView(c: .purple, plan: "Basic", price: 9, fontColor: .white)
+                PricingView(c: .mint, plan: "Pro", price: 19, fontColor: .black)
+            }
+            PricingView(c: .yellow, plan: "Team", price: 199, fontColor: .black)
         }
-        .padding()
+    }
+}
+
+struct PricingView: View {
+    var c: Color
+    var plan: String
+    var price: Int
+    var fontColor: Color
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .fill(c)
+                .padding(10)
+            VStack{
+                Text(plan)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(fontColor)
+                Text("$" + String(price))
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(fontColor)
+                Text("per month")
+                    .font(.headline)
+                    .foregroundStyle(fontColor)
+            }
+        }
     }
 }
 
